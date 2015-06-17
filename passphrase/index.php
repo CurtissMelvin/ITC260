4897a481f5a5a173c5d126139ae5c29671f768b1
Curtiss Melvin <CurtissMelvin@users.noreply.github.com>
2015-06-16 22:45:07 -0700

    index.php

---
+++ passphrase/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * 
+ * @package ITC260
+ * @author Curtiss <curtissm@hotmail.com>
+ * @version 1.0 2015/06/16 
+ * @link http://www.curtissmelvin.com
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @Passphrase Assignment A3:
+ */
+
+Include "passphrase.php";
+?>
+<!DOCTYPE HTML>
+	<html>
+		<body>
+				<p>You are logged in!</p>
+		</body>
+	
+	</html>
